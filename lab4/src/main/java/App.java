6e5b92dba653ec02c3cb17bdb4ed7e568fa6e935
Diana Artiom <diana.artiom24@gmail.com>
2017-12-12 00:02:41 +0300

    Configured Logger.

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -1,2 +1,9 @@
+import org.apache.log4j.Logger;
+
 public class App {
+
+    public static void main(String[] args) {
+        final Logger LOGGER = Logger.getLogger(App.class);
+        LOGGER.info("Logger successfully configured.");
+    }
 }
