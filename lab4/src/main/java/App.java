92c5d36c6d5bb2bdca8969b4c83afdac8a1ec556
Diana Artiom <diana.artiom24@gmail.com>
2017-12-12 01:21:06 +0300

    Implemented POST.

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -1,9 +1,18 @@
+import handler.PostHandler;
+import http.Server;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 public class App {
 
-    public static void main(String[] args) {
-        final Logger LOGGER = Logger.getLogger(App.class);
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+
+    private static PostHandler postHandler;
+    private static Server server;
+    public static void main(String[] args) throws IOException {
         LOGGER.info("Logger successfully configured.");
+        server = new Server();
+        server.run();
     }
 }
