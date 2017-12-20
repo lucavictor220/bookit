a103de8e273d575f4a1f1178e0dd91077e56f09b
dianaartiom <diana.artiom24@gmail.com>
2017-12-20 23:40:11 +0300

    Developed more functionality for the book controller.

--- app/src/main/java/utm/pad/lab3/controllers/BookController.java
+++ app/src/main/java/utm/pad/lab3/controllers/BookController.java
@@ -1,4 +1,40 @@
 package utm.pad.lab3.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import utm.pad.lab3.builders.BookBuilder;
+import utm.pad.lab3.dtos.BookDTO;
+import utm.pad.lab3.services.BookService;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Controller
+@RequestMapping("/book")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpEntity<?> getAllBooks() {
+        return new ResponseEntity(bookService.findAll(), OK);
+    }
+
+    /* pentru mine ca sa pot introduce carti */
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public HttpEntity<?> createBook(@RequestBody BookDTO bookDTO) {
+
+        return new ResponseEntity(bookService.insert(BookBuilder.get(bookDTO)), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpEntity<?> getBookById(@PathVariable String id) {
+        return new ResponseEntity(bookService.findById(id), OK);
+    }
+
 }
